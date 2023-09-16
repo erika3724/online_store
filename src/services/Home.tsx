c7cfd1c8c9b420b5c81ad4aecdb87e00a4802f23
Evandro <evandrosantossjr@gmail.com>
2023-09-16 20:39:15 -0300

    requisito 1 ao 3

--- src/services/Home.tsx
+++ src/services/Home.tsx
@@ -8,6 +8,7 @@ function Home() {
       <h2 data-testid="home-initial-message">
         Digite algum termo de pesquisa ou escolha uma
         categoria.
+        <Carrinho />
       </h2>
     </div>
   );
